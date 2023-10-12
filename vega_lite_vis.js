2ce4e9d775ee19aadcfd4a83b3189cf4deaa8253
ntan0033 <110961574+ntan0033@users.noreply.github.com>
2023-10-13 04:29:52 +0800

    pain n suffering
    i cry

---
+++ vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "map.json";
+vegaEmbed("#chloropleth", vg_1).then(function(result) {}).catch(console.error);
+var vg_2 = "index_bar_100.json"
+vegaEmbed("#index_bar", vg_2).then(function(result) {}).catch(console.error);
+var vg_3 = "year_line.json"
+vegaEmbed("#year_line", vg_3).then(function(result) {}).catch(console.error);
