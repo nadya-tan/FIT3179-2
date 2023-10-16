6076e81df42013c22de2a868095a65fee0e5f76f
ntan0033 <110961574+ntan0033@users.noreply.github.com>
2023-10-16 13:46:05 +0800

    Recent
    changes

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,22 @@
 var vg_1 = "map.json";
-vegaEmbed("#chloropleth", vg_1).then(function(result) {}).catch(console.error);
-var vg_2 = "index_bar_100.json"
-vegaEmbed("#index_bar", vg_2).then(function(result) {}).catch(console.error);
-var vg_3 = "year_line.json"
-vegaEmbed("#year_line", vg_3).then(function(result) {}).catch(console.error);
+vegaEmbed("#chloropleth", vg_1, { config: { background: "#dadada" } })
+  .then(function (result) {})
+  .catch(console.error);
+var vg_2 = "index_bar_100.json";
+vegaEmbed("#index_bar", vg_2, {
+  config: {
+    background: "#dadada",
+    axis: { gridColor: "#fafafa" },
+  },
+})
+  .then(function (result) {})
+  .catch(console.error);
+var vg_3 = "year_line.json";
+vegaEmbed("#year_line", vg_3, {
+  config: {
+    background: "#dadada",
+    axis: { gridColor: "#fafafa" },
+  },
+})
+  .then(function (result) {})
+  .catch(console.error);
